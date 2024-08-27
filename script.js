4a1706917c06c8a6e7813f50c69000d23f8d1044
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2024-08-27 19:55:59 +0545

    the final commit -provided code works everything is debugged

--- script.js
+++ script.js
@@ -18,59 +18,44 @@ function getHumanChoice(){
 }
 
 function playRound(humanChoice,computerChoice){
-    if(humanScore >= 3){
-        console.log("Human wins the round")
-    }
-    else if(computerScore >= 3){
-        console.log("Computer wins the round")
-    }
-    else if(humanChoice === "rock" && computerChoice === "rock"){
-        console.log("Its a draw");
+    if(humanChoice === "rock" && computerChoice === "rock"){
         console.log("The score of computer is " + computerScore);
         console.log("The score of human is " + humanScore);
     }
     else if(humanChoice === "rock" && computerChoice === "paper"){
-        console.log("Computer Wins");
         computerScore = computerScore + 1;
         console.log("The score of computer is " + computerScore);
         console.log("The score of human is " + humanScore);
     }
     else if(humanChoice === "rock" && computerChoice === "scissors"){
-        console.log("Human Wins");
         humanScore = humanScore + 1;
         console.log("The score of computer is " + computerScore);
         console.log("The score of human is " + humanScore);
     }
     else if(humanChoice === "paper" && computerChoice === "paper"){
-        console.log("Its a draw");
         console.log("The score of computer is " + computerScore);
         console.log("The score of human is " + humanScore);
     }
     else if(humanChoice === "paper" && computerChoice === "rock"){
-        console.log("Human Wins");
         humanScore = humanScore + 1;
         console.log("The score of computer is " + computerScore);
         console.log("The score of human is " + humanScore);
     }
     else if(humanChoice === "paper" && computerChoice === "scissors"){
-        console.log("Computer wins");
         computerScore = computerScore + 1;
         console.log("The score of computer is " + computerScore);
         console.log("The score of human is " + humanScore);
     }
     else if(humanChoice === "scissors" && computerChoice === "scissors"){
-        console.log("Its a draw");
         console.log("The score of computer is " + computerScore);
         console.log("The score of human is " + humanScore);
     }
     else if(humanChoice === "paper" && computerChoice === "rock"){
-        console.log("Human Wins");
         humanScore = humanScore + 1;
         console.log("The score of computer is " + computerScore);
         console.log("The score of human is " + humanScore);
     }
     else if(humanChoice === "paper" && computerChoice === "scissors"){
-        console.log("Computer Wins");
         computerScore = computerScore + 1;
         console.log("The score of computer is " + computerScore);
         console.log("The score of human is " + humanScore);
@@ -79,7 +64,19 @@ function playRound(humanChoice,computerChoice){
 }
 
 function playGame(){
-    for(let i =0; i<=7; i++){playRound(getHumanChoice(),getComputerChoice())};
-}
+    for(let i = 0; i<=10; i++){
+        playRound(getHumanChoice(),getComputerChoice())
+        if(computerScore > 2 || humanScore > 2){
+                if(humanScore > computerScore){
+                    console.log("Human Wins The round");
+                }
+                else if(humanScore < computerScore){
+                    console.log("Computer wins the round")
+                }
+                break;
+            }
+        }
+    };
+
 
 playGame()
